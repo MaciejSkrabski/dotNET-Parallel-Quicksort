71222e986caca83f66df5e5cdb7e43504a044984
MaciejSkrabski <maciej.skrabski@gmail.com>
2020-06-10 17:16:59 +0200

    Initial commit

--- QuickSort and Parallel QuickSort/Program.cs
+++ QuickSort and Parallel QuickSort/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace QuickSort_and_Parallel_QuickSort
 {
@@ -10,6 +8,59 @@ namespace QuickSort_and_Parallel_QuickSort
     {
         static void Main(string[] args)
         {
+            int j = 10000;
+            for (int k=0; k<=7; k++)
+            {
+                Console.WriteLine($"=======\n {j} elements");
+                int[] tab = new int[j];
+                Console.WriteLine("Creating array of random integers");
+                Random rnd = new Random();
+                for (int i = 0; i < j; i++)
+                    tab[i] = rnd.Next(-10000000, 10000000);
+
+                int[] tab2 = new int[j];
+                int[] tab3 = new int[j];
+                Array.Copy(tab, 0, tab2, 0, j);
+                Array.Copy(tab, 0, tab3, 0, j);
+
+
+                Console.WriteLine("continue\n");
+                Array.Sort(tab);
+                Console.WriteLine("tab2 sorted? " + tab2.SequenceEqual(tab));
+                Console.WriteLine("Sequentally sorting the array. Please be patient.\n");
+                // Porównanie złożoności czasowej
+
+                Stopwatch sw = new Stopwatch();
+                sw.Reset();
+                sw.Start();
+                QuickSort.SequentialSort(tab2, 0, j - 1);
+                sw.Stop();
+                TimeSpan seq = sw.Elapsed;
+                sw.Reset();
+                Console.WriteLine("Sequential Quicksort Time: " + seq);
+                Console.WriteLine("tab2 sorted? " + tab2.SequenceEqual(tab));
+
+                Console.WriteLine("tab3 sorted? " + tab3.SequenceEqual(tab));
+                Console.WriteLine("Paralelly sorting the array. Please be patient.\n");
+
+                sw.Start();
+                QuickSort.ParallelSort(tab3, 0, j - 1);
+                sw.Stop();
+                TimeSpan par = sw.Elapsed;
+                sw.Reset();
+                Console.WriteLine("Parallel Quicksort Time: " + par);
+                Console.WriteLine("tab3 sorted? " + tab3.SequenceEqual(tab));
+
+                sw.Reset();
+
+                Console.WriteLine("\n\n\n" + (seq - par));
+
+                j *= 5;
+            }
+            
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
         }
     }
 }
---
+++ QuickSort and Parallel QuickSort/QuickSort.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace QuickSort_and_Parallel_QuickSort
+{
+    static class QuickSort
+    {
+        public static void Swap<T>(IList<T> arr, int i, int j)
+        {             ///     Zamienia wartości tablicy leżące na indeksach i,j             
+            var tmp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = tmp;
+        } 
+
+        public static int Partition<T>(IList<T> arr, int low, int high) where T : IComparable<T>
+        {             
+        ///Dzieli tablicę na wycinki i porównuje ze sobą wycinki             
+        //////Podział wybrany po środku tablicy            
+        int pivotPos = (high + low) / 2;
+        T pivot = arr[pivotPos];
+        /// zamiana dla uproszczenia pętli             
+        Swap(arr, low, pivotPos);
+        int left = low;
+        for (var i = low + 1; i <= high; i++) {
+            if (arr[i].CompareTo(pivot) >= 0) continue;
+            left++; 
+            Swap(arr, i, left);
+            }
+        Swap(arr, low, left); 
+        return left;
+        }
+
+        /// Sekwencyjny Quicksort         
+        public static void SequentialSort<T>(IList<T> arr, int left, int right) where T : IComparable<T>
+        {
+            if (right <= left) return; 
+            var pivot = Partition(arr, left, right);
+            SequentialSort(arr, left, pivot - 1); ///na lewo od podziału             
+            SequentialSort(arr, pivot + 1, right); ///na prawdo od podziału         
+        } 
+
+        ///Zrównoleglony Quicksort
+        public static void ParallelSort<T>(IList<T> arr, int left, int right) where T : IComparable<T>
+        {
+            if (right <= left) return;
+            /// Podział                 
+            var pivot = Partition(arr, left, right);
+            /// Zrównoleglone sortowanie                 
+            System.Threading.Tasks.Parallel.Invoke( 
+                () => ParallelSort(arr, left, pivot - 1), 
+                () => ParallelSort(arr, pivot + 1, right)
+            );
+        }     
+    }
+}
